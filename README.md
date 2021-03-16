00356b3298387366c47ccc14fcc3429db0712a17
Karina Finegan <kairosity@users.noreply.github.com>
2021-03-16 19:56:08 +0000

    struc: further restructuring the app

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # REST API Blog
-
 A blog application that interacts with faux REST API: JSON Server.
+<br>
 
-# Libraries / Frameworks
+# Viewing/Using this Application in a Development Environment
 
-- Bootstrap
-- [Nunjucks](https://mozilla.github.io/nunjucks/)
+### 1. Type __npm install__ in a terminal window to install dependencies.
+
+### 2. Type __npm run api__ in a terminal window to start the JSON Server REST API.
+
+### 3. Type __nodemon app__ in a separate terminal to run the blog application.
+
+### 4. Navigate to __http://localhost:3000/__ to view the application.
+<br>
+
+# Libraries / Frameworks / Tech Used
+
+## - [Node.js](https://nodejs.org/en/)
+## - [Express.js](https://expressjs.com/)
+## - [npm](https://www.npmjs.com/)
+## - [EJS](https://mozilla.github.io/nunjucks/)
+## - [Bootstrap](https://getbootstrap.com/)
--- app.js
+++ app.js
@@ -2,15 +2,9 @@ const { render } = require('ejs');
 const express = require('express');
 const morgan = require('morgan');
 const fetch = require('node-fetch');
-// const get_articles = require('./js/index')
 
-// set up express app
 const app = express();
-
-// register view engine
 app.set('view engine', 'ejs');
-
-// listen for requests
 app.listen(3000);
 
 // middleware & static files
@@ -20,23 +14,47 @@ app.use(morgan('dev'));
 // Homepage View
 app.get('/', async (req, res) => {
 
-    let uri = 'http://localhost:9000/posts?_sort=publish_date&_order=desc';
+    async function getAllBlogPosts(){
+        let uri = 'http://localhost:9000/posts?_sort=publish_date&_order=desc';
+        const fetch_res = await fetch(uri);
+        let posts = await fetch_res.json();
+        return posts
+    }
+    const posts = await getAllBlogPosts();
 
-    const fetch_res = await fetch(uri);
-    let articles = await fetch_res.json();
-    console.log(articles)
-
-    res.render('index', { title: "Health Blog", articles: articles });
+    res.render('index', { title: "Health Blog", posts: posts });
 });
 
 // About Page View
 app.get('/about', (req, res) => {
-    res.render('about', { title: "About Us" });
+    res.render('about', { title: "About" });
 });
 
-// Create New Blog View
-app.get('/blogs/create', (req, res) => {
+// Blog Details View
+app.get('/blog/create', (req, res) => {
     res.render('create', { title: "Create New Blog Post" })
+});
+
+// Create New Blog Post View - need to get slug from the post object on the index template.
+app.get('/blog/:slug', async (req, res) => {
+    console.log(req)
+    // If req.method == GET
+    const slug = req.params.slug;
+
+    console.log(`Slug is ${slug}`)
+
+    // Go through all blogs and pull out the one with this as its slug
+    async function getPost(){
+        let uri = `http://localhost:9000/posts?slug=${slug}`;
+        const fetch_res = await fetch(uri);
+        let post = await fetch_res.json();
+        console.log(post)
+        return post
+    }
+    const post = await getPost();
+    
+    res.render('get_blog_post', { post: post, title: "Blog Post" });
+    
 });
 
 // 404 Page View
